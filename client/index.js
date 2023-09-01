b0b9bf3f6aa7b3643cd1adff8fecf6c22200998f
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2023-09-01 10:41:22 -0500

    All the changes to solve the exercise

--- client/index.js
+++ client/index.js
@@ -6,9 +6,14 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const name = "Alexander Franey";
+  const index = niceList.findIndex(n => n === name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof: merkleTree.getProof(index),
   });
 
   console.log({ gift });
